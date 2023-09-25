a07f4c4dc6e95d5c16727787ce204c7b8d615de1
Rajeev-katiyar <138558221+Rajeev-katiyar@users.noreply.github.com>
2023-09-25 16:19:45 +0530

    some issue in code

--- Django_web_application/file_uploading/models.py
+++
@@ -1,7 +0,0 @@
-
-
-# Create your models here.
-from django.db import models
-
-class img_uploader(models.Model):
-    imageFile = models.FileField(upload_to='uploader/')  # Use FileField for file uploads
--- Django_web_application/login/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class logindata(models.Model):
-    email=models.EmailField(null=True)
-    password=models.CharField(max_length=30,null=True)
